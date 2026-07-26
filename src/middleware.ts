661947980c0af170404e4b762ac0854057ea55e5
Umesh Vashist <us781819@gmail.com>
2026-07-26 10:47:04 +0530

    DevStore 0.7

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,7 @@ const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/api/auth/google/callback(.*)",
+  "/__clerk/(.*)",
 ]);
 
 const isAuthPage = createRouteMatcher([
